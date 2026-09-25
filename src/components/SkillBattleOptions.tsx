30dd91c302eb241e496608fca7e0a373000c1391
pawich12171-afk <pawich12171@kpsp.ac.th>
2026-09-25 23:51:48 +0700

    Fix skill stat type import for minion editor

--- src/components/SkillBattleOptions.tsx
+++ src/components/SkillBattleOptions.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { BattleBotSkill, BattleSkillCategory, BattleSkillTarget, BattleSkillEffectKind, BattleSkillModifier, CharacterStats, Skill } from '../types';
+import type { BattleBotSkill, BattleSkillCategory, BattleSkillTarget, BattleSkillEffectKind, BattleSkillModifier, BattleSkillStatKind, CharacterStats, Skill } from '../types';
 
 type SummonUnit = NonNullable<Skill['summonUnits']>[number];
 
